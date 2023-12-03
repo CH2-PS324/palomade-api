59a6ddbac75dcca47ddeb96169a22b93ccb9ebdd
Hafiz Caniago <hafizcode02@gmail.com>
2023-12-03 14:31:59 +0700

    add users route

---
+++ src/routes/user.route.js
@@ -0,0 +1,39 @@
+const { authJwt, verifyUser } = require("../middleware");
+const controller = require("../controllers/user.controller");
+
+module.exports = function (app) {
+    app.use(function (req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
+
+    // Register User
+    app.post(
+        "/api/users/register",
+        [verifyUser.checkDuplicateEmail],
+        controller.create
+    );
+
+    // Login User
+    app.post("/api/users/login", controller.login);
+
+    // Refresh Token
+    app.post("/api/users/refreshtoken", controller.refreshToken);
+
+    // Get User Detail
+    app.get("/api/users/:id", [authJwt.verifyToken], controller.getOne);
+
+    // Update User
+    app.patch(
+        "/api/users/:id",
+        [
+            authJwt.verifyToken,
+            verifyUser.checkDuplicateEmail,
+            verifyUser.checkRolesExisted,
+        ],
+        controller.update
+    );
+};
