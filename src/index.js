46c9c6c31b8893244fc50aae31768d2099439def
Iqbal Pamula Ba'iq <balpamula@gmail.com>
2023-12-04 13:03:12 +0700

    rollback SERVER_PORT to PORT

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const startServer = async () => {
         await initializeDatabase();
 
         // Start server
-        const PORT = process.env.SERVER_PORT || 8080;
+        const PORT = process.env.PORT || 8080;
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}.`);
         });
