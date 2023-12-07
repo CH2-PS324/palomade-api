ac959eaa1bbedbfe5f291ef65d0f95f88da9261d
Hafiz Caniago <hafizcode02@gmail.com>
2023-12-07 21:53:15 +0700

    fix only org user can hit shipping create endpoint

--- src/routes/shipping.route.js
+++ src/routes/shipping.route.js
@@ -1,40 +1,15 @@
 const controller = require("../controllers/shipping.controller.js");
-const { verifyUser } = require("../middleware");
-
-// const { registerValidator, runValidaton } = require('../validation/user.validation')
+const { verifyUser, authJwt } = require("../middleware");
 
 module.exports = function (app) {
-    // app.use(function (req, res, next) {
-    //     res.header(
-    //         "Access-Control-Allow-Headers",
-    //         "x-access-token, Origin, Content-Type, Accept"
-    //     );
-    //     next();
-    // });
+    app.use(function (req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
 
     // Create Shipping
-    app.post("/api/shipping/create", verifyUser.checkDriverId, controller.create);
-
-    // Login User
-    // app.post("/api/users/login", controller.login);
-
-    // // Refresh Token
-    // app.post("/api/users/refreshtoken", controller.refreshToken);
-
-    // // Get User Detail
-    // app.get("/api/users/:id", [authJwt.verifyToken], controller.getOne);
-
-    // // Update User
-    // app.patch(
-    //     "/api/users/:id",
-    //     [
-    //         authJwt.verifyToken,
-    //         verifyUser.checkDuplicateEmail,
-    //         verifyUser.checkRolesExisted,
-    //     ],
-    //     controller.update
-    // );
-
-    // Send Email Example API
-    // app.post("/api/users/sendmail", controller.sendWelcomeEmail);
+    app.post("/api/shipping/create", [authJwt.verifyToken, authJwt.isOrganisasi], controller.create);
 };
