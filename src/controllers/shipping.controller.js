6cb5ee20c63392d3618bdf36b8e12b948f047499
Hafiz Caniago <hafizcode02@gmail.com>
2023-12-07 21:52:29 +0700

    deleted unused comment

--- src/controllers/shipping.controller.js
+++ src/controllers/shipping.controller.js
@@ -1,14 +1,6 @@
 const { db } = require('../models');
 const { shipping: Shipping } = db;
 const { v4: uuidv4 } = require('uuid');
-// const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcryptjs');
-// const config = require('../config/auth.config');
-// const Hashids = require('hashids/cjs');
-// const hashids = new Hashids(process.env.HASH_KEY, 16)
-
-// const { mailWelcomeTemplate } = require('../utils/mailtemplate.utils');
-// const { kirimEmail } = require('../utils/mailsender.utils');
 require("dotenv").config();
 
 exports.create = async (req, res) => {
