34f20031020c87b08404f5b80661ae474f005b59
Iqbal Pamula Ba'iq <balpamula@gmail.com>
2023-12-13 11:13:52 +0700

    refactor subscription

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -23,47 +23,44 @@ exports.reedem = async (req, res) => {
         }
 
         // Check if subscription code is not found or has maximum reedem or has expired or is non-active code
-        Subscription.findOne({
+        const subscription = await Subscription.findOne({
             where: {
                 code: req.body.code
             }
-        }).then((subscription) => {
-            if (!subscription || subscription.quota < 1 || (subscription.is_active == false) || new Date(subscription.expiredAt) < new Date()) {
-                return res.status(404).send({
-                    message: "subscription code not found, already have maximum reedem, or already expired"
-                })
-            }
-
-            // creating subscription detail record
-            Subscription_detail.create({
-                id_subscription: subscription.id,
-                id_user: req.userId,
-                subsExpiredAt: plus30day(30)
-            })
-
-            // change attribute is_subscribe in user
-            User.update({
-                is_subscribe: true,
-            }, {
-                where: {
-                    id: req.userId
-                }
-            })
-
-            // change count of quota - 1
-            Subscription.update({
-                quota: subscription.quota - 1
-            }, {
-                where: {
-                    id: subscription.id
-                }
-            })
-
-            res.status(200).send({
-                message: "Code successfully redeemed.",
-                data: req.body
-            });
         })
+
+        if (!subscription || subscription.quota < 1 || (subscription.is_active == false) || new Date(subscription.expiredAt) < new Date()) {
+            return res.status(404).send({
+                message: "subscription code not found, already have maximum reedem, or already expired"
+            })
+        }
+
+        Subscription_detail.create({
+            id_subscription: subscription.id,
+            id_user: req.userId,
+            subsExpiredAt: plus30day(30)
+        })
+
+        User.update({
+            is_subscribe: true,
+        }, {
+            where: {
+                id: req.userId
+            }
+        })
+
+        Subscription.update({
+            quota: subscription.quota - 1
+        }, {
+            where: {
+                id: subscription.id
+            }
+        })
+
+        res.status(200).send({
+            message: "Code successfully redeemed.",
+            data: req.body
+        });
     } catch (err) {
         console.error(err.message);
         if (err.message.includes("invalid input syntax")) {
